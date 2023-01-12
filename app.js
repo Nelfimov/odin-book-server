1cb44703b628e93c2ff3aee6d07f0fd37ecda85f
nelfimov <nelfimov@mail.ru>
2023-01-12 10:22:45 +0300

    refactoring

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 import createError from 'http-errors';
 import express, {json, urlencoded} from 'express';
-// import {join, dirname} from 'path';
-// import {fileURLToPath} from 'url';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import * as dotenv from 'dotenv';
@@ -16,19 +14,11 @@ const app = express();
 
 connectMongoose(process.env.MONGODB_URL);
 
-// view engine setup
-/*
-const __dirname = dirname(fileURLToPath(import.meta.url));
-app.set('views', join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-*/
-
 app.use(logger('dev'));
 app.use(json());
 app.use(passport.initialize());
 app.use(urlencoded({extended: false}));
 app.use(cookieParser());
-// app.use(express.static(join(__dirname, 'public')));
 
 app.use('/', startRouter);
 app.use('/auth', authRouter);
